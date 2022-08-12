95e67885f60ad585bc6ecf92f999572cbd7f0cd3
Wackierleaf <wackier.leaf@yandex.ru>
2022-08-12 11:01:51 +0300

    Finished 3 from chat refactoring and fix error with avatar

--- lib/features/chat/screens/chat_screen.dart
+++ lib/features/chat/screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:surf_practice_chat_flutter/features/chat/models/chat_geolocation_geolocation_dto.dart';
 import 'package:surf_practice_chat_flutter/features/chat/models/chat_message_dto.dart';
 import 'package:surf_practice_chat_flutter/features/chat/models/chat_message_location_dto.dart';
 import 'package:surf_practice_chat_flutter/features/chat/models/chat_user_dto.dart';
@@ -48,7 +50,7 @@ class _ChatScreenState extends State<ChatScreen> {
               messages: _currentMessages,
             ),
           ),
-          _ChatTextField(onSendPressed: _onSendPressed),
+          _ChatTextField(onSendPressed: _onSendPressed, onSendGeoMsg: _onSendGeoMsg,),
         ],
       ),
     );
@@ -61,8 +63,15 @@ class _ChatScreenState extends State<ChatScreen> {
     });
   }
 
-  Future<void> _onSendPressed(String messageText) async {
-    final messages = await widget.chatRepository.sendMessage(messageText);
+  Future<void> _onSendPressed(String msg) async {
+    final messages = await widget.chatRepository.sendMessage(msg);
+    setState(() {
+      _currentMessages = messages;
+    });
+  }
+
+  Future<void> _onSendGeoMsg(ChatGeolocationDto location, String msg) async {
+    final messages = await widget.chatRepository.sendGeolocationMessage(location: location, message: msg);
     setState(() {
       _currentMessages = messages;
     });
@@ -90,11 +99,13 @@ class _ChatBody extends StatelessWidget {
 
 class _ChatTextField extends StatelessWidget {
   final ValueChanged<String> onSendPressed;
+  final onSendGeoMsg;
 
   final _textEditingController = TextEditingController();
 
   _ChatTextField({
     required this.onSendPressed,
+    required this.onSendGeoMsg,
     Key? key,
   }) : super(key: key);
 
@@ -103,16 +114,90 @@ class _ChatTextField extends StatelessWidget {
     final mediaQuery = MediaQuery.of(context);
     final colorScheme = Theme.of(context).colorScheme;
 
+    Future<Position> _determinePosition() async {
+      bool serviceEnabled;
+      LocationPermission permission;
+
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
+      }
+
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+      return await Geolocator.getCurrentPosition();
+    }
+
+    void setLocationIntoChatTextField() async {
+      Position position = await _determinePosition();
+      _textEditingController.text = 'Сообщение с геолокацией: (${position.longitude}, ${position.latitude})';
+      ChatGeolocationDto location = ChatGeolocationDto(latitude: position.latitude, longitude: position.longitude);
+      onSendGeoMsg(location,_textEditingController.text);
+    }
+
+    void showAttachmentsDialog() async {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: Text('Вложения'),
+                content: Wrap(
+                  verticalDirection: VerticalDirection.down,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {Navigator.pop(context, 'Picture');},
+                      icon: const Icon(
+                        Icons.image,
+                        color: Colors.grey,
+                        size: 72,
+                      ),
+                      label: const Text('Отправить картинки'),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {
+                        setLocationIntoChatTextField();
+                        Navigator.pop(context, 'Geo');
+                      },
+                      icon: const Icon(
+                        Icons.pin_drop,
+                        color: Colors.blue,
+                        size: 72,
+                      ),
+                      label: const Text('Отправить геолокацию'),
+                    )
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.pop(context, 'Close'),
+                      child: const Text('Закрыть'))
+                ],
+              ));
+    }
+
     return Material(
       color: colorScheme.surface,
       elevation: 12,
       child: Padding(
         padding: EdgeInsets.only(
           bottom: mediaQuery.padding.bottom + 8,
-          left: 16,
+          left: 5,
         ),
         child: Row(
           children: [
+            IconButton(
+                onPressed: () => showAttachmentsDialog(),
+                splashRadius: 25,
+                icon: const Icon(Icons.attach_file)),
             Expanded(
               child: TextField(
                 controller: _textEditingController,
@@ -125,6 +210,7 @@ class _ChatTextField extends StatelessWidget {
               onPressed: () => onSendPressed(_textEditingController.text),
               icon: const Icon(Icons.send),
               color: colorScheme.onSurface,
+              splashRadius: 25,
             ),
           ],
         ),
@@ -168,8 +254,10 @@ class _ChatMessage extends StatelessWidget {
   }) : super(key: key);
 
   void launchMapsApp(double? longitude, double? latitude) async {
-    if (await canLaunchUrlString('https://yandex.ru/maps/?ll=$longitude%2C$latitude&z=2')) {
-      await launchUrlString('https://yandex.ru/maps/?ll=$longitude%2C$latitude&z=2');
+    if (await canLaunchUrlString(
+        'https://yandex.ru/maps/?ll=$longitude%2C$latitude&z=2')) {
+      await launchUrlString(
+          'https://yandex.ru/maps/?ll=$longitude%2C$latitude&z=2');
     }
   }
 
@@ -198,7 +286,9 @@ class _ChatMessage extends StatelessWidget {
                       ),
                       if (chatData is ChatMessageGeolocationDto)
                         IconButton(
-                          onPressed: () => launchMapsApp(chatData.location?.longitude, chatData.location?.latitude),
+                          onPressed: () => launchMapsApp(
+                              chatData.location?.longitude,
+                              chatData.location?.latitude),
                           icon: const Icon(
                             Icons.location_on,
                             color: Colors.blue,
@@ -251,6 +341,16 @@ class _ChatAvatar extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
+    String getInitials() {
+      if (userData.name != null) {
+        var splitted = userData.name!.split(' ');
+        return splitted.last.isNotEmpty
+            ? '${splitted.first[0]}${splitted.last[0]}'
+            : splitted.first[0];
+      }
+      return '';
+    }
+
     return SizedBox(
       width: _size,
       height: _size,
@@ -259,9 +359,7 @@ class _ChatAvatar extends StatelessWidget {
         shape: const CircleBorder(),
         child: Center(
           child: Text(
-            userData.name != null
-                ? '${userData.name!.split(' ').first[0]}${userData.name!.split(' ').last[0]}'
-                : '',
+            getInitials(),
             style: TextStyle(
               color: colorScheme.onPrimary,
               fontWeight: FontWeight.bold,
